026ce80dd336f4cd44106f22c37623e2129c36cb
termnml <termnml@gmail.com>
2021-11-14 14:22:56 +0100

    separated [configs_basic]: nvim, bash, iex

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # get absolute path to dotfiles folder
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# if no argument given, output helf and exit
+# if no argument given, output help and exit
 if [[ ! $# -gt 0 ]]; then
 	echo "use:"
 	echo "install.sh (-a|--all)"
@@ -12,7 +12,10 @@ if [[ ! $# -gt 0 ]]; then
 	echo "install.sh (-p|--packages)"
 	echo " - installs packages"
 	echo "install.sh (-c|--configs)"
-	echo " - installs symlinks to configs"
+	echo " - installs symlinks to all configs"
+	echo "install.sh (-cb|--configs-basic)"
+	echo " - installs symlinks to the basic configs"
+	echo " - like nvim, bash, elixir"
 	exit 0
 fi
 
@@ -33,6 +36,11 @@ while test $# -gt 0; do
 			;;
 		-c|--configs)
 			action[configs]=true
+			action[configs_basic]=true
+			shift
+			;;
+		-cb|--configs-basic)
+			action[configs_basic]=true
 			shift
 			;;
 		*)
@@ -49,8 +57,15 @@ if [[ ${action[packages]} = true ]]; then
 fi
 
 SET_SYMLINK () {
+	# get parentdir of file
+	PARENT_DIR="$(dirname $HOME/$1)"
 	# returns if symlink was created and action_after should be called
 	if [[ ! -L $HOME/$1 ]]; then
+		# create parent-dir if not present
+		if [[ ! -d ${PARENT_DIR} ]]; then
+			mkdir -p ${PARENT_DIR}
+		fi
+		# check if file already exists and backup
 		if [[ -f $HOME/$1 ]]; then
 			mv $HOME/$1 $HOME/$1_pre_dotfile
 		fi
@@ -60,35 +75,6 @@ SET_SYMLINK () {
 }
 
 if [[ ${action[configs]} = true ]]; then
-	# nvim
-	TARGET='.config/nvim/init.vim'
-	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
-		echo "[new symlink] ${TARGET}"
-		# additional steps here
-		if [[ ! -d $HOME/.config/nvim/bundle/Vundle.vim ]]; then
-			git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-			nvim +PluginInstall +qall
-		fi
-	else
-		echo "[already symlinked] ${TARGET}"
-	fi
-	TARGET='.local/share/applications/nvim.desktop'
-	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
-		echo "[new symlink] ${TARGET}"
-		# additional steps here
-	else
-		echo "[already symlinked] ${TARGET}"
-	fi
-
-	# bash
-	TARGET='.bashrc'
-	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
-		echo "[new symlink] ${TARGET}"
-		# additional steps here
-	else
-		echo "[already symlinked] ${TARGET}"
-	fi
-
 	#ranger
 	TARGET='.config/ranger/rc.conf'
 	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
@@ -139,6 +125,39 @@ if [[ ${action[configs]} = true ]]; then
 	if [[ ! -d ~/.config/termite ]]; then
 		mkdir ~/.config/termite
 	fi
+	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
+		echo "[new symlink] ${TARGET}"
+		# additional steps here
+	else
+		echo "[already symlinked] ${TARGET}"
+	fi
+
+fi
+
+
+if [[ ${action[configs_basic]} = true ]]; then
+	# nvim
+	TARGET='.config/nvim/init.vim'
+	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
+		echo "[new symlink] ${TARGET}"
+		# additional steps here
+		if [[ ! -d $HOME/.config/nvim/bundle/Vundle.vim ]]; then
+			git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+			nvim +PluginInstall +qall
+		fi
+	else
+		echo "[already symlinked] ${TARGET}"
+	fi
+	TARGET='.local/share/applications/nvim.desktop'
+	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
+		echo "[new symlink] ${TARGET}"
+		# additional steps here
+	else
+		echo "[already symlinked] ${TARGET}"
+	fi
+
+	# bash
+	TARGET='.bashrc'
 	if [[ "$(SET_SYMLINK ${TARGET})" == "symlink_created" ]]; then
 		echo "[new symlink] ${TARGET}"
 		# additional steps here
